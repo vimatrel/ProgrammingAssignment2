dc7d9c147a4f99dc350372450f61318ded8040d1
vimatrel <vimatrel@outlook.com>
2015-01-23 18:06:08 -0600

    Changed comment structure

--- cachematrix.R
+++ cachematrix.R
@@ -7,45 +7,43 @@
 
 
 ## makeCacheMatrix provides a set of functions that assist in the management
-## of the matrix and the cache as described next.
-## set: assigns a new matrix to x
-## get: provides the current matrix stored in x
-## setinv: assigns the computed inverse matrix to inv to be used as cache
-## getinv: provides the current value of inv. Will be NULL if inverse for current 
-## matrix has not been computed before
-
+## of the matrix and the cache.
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
+        ## set: assigns a new matrix to x
         set <- function(y) {
                 x <<- y
                 inv <<- NULL
         }
+        ## get: provides the current matrix stored in x
         get <- function() x
+        ## setinv: assigns the computed inverse matrix to inv to be used as cache
         setinv <- function(inverse) inv <<- inverse
+        ## getinv: provides the current value of inv. Will be NULL if inverse for current 
+        ## matrix has not been computed before
         getinv <- function() inv
         list(set = set, get = get, 
              setinv = setinv, getinv = getinv)               
 }
 
 
-## cacheSolve: Using the functions provided by makeCacheMatrix
-## validates if inverse has been computed before by calling getinv() and
-## checking it´s current value. If current matrix inverse has already been 
-## computed, returns cached value with proper message. 
-## if not previously computed, uses get() to retrieve current matrix and
-## computes the inverse. 
-## uses setinv() to store computation in cache
-## prints the computation.
-
+## cacheSolve: Uses the functions provided by makeCacheMatrix and
+## computes the inverse of the special "matrix".
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## validate if inverse has been computed before by calling getinv() and
+        ## check it´s current value. If current matrix inverse has already been 
+        ## computed, return cached value with proper message.
         inv <- x$getinv()
         if (!is.null(inv)) {
                 message("getting cached data")
                 return(inv)
         }
+        ## if not previously computed, use get() to retrieve current matrix and
+        ## compute the inverse. 
         data <- x$get()
         inv <- solve(data, ...)
+        ## use setinv() to store computation in cache
         x$setinv(inv)
+        ## display the computation.
         inv
 }
